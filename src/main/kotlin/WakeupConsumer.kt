ccdfab3871df6b4fee64d96369079f59e40a1b12
boki <lsb530@naver.com>
2024-09-20 19:35:22 +0900

    graceful kafka consumer close(kt)

---
+++ src/main/kotlin/WakeupConsumer.kt
@@ -0,0 +1,69 @@
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.apache.kafka.clients.consumer.ConsumerConfig.*
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.errors.WakeupException
+import org.apache.kafka.common.serialization.Deserializer
+import org.apache.kafka.common.serialization.StringDeserializer
+import java.time.Duration
+import java.util.*
+import kotlin.concurrent.thread
+
+class WakeupConsumer {
+
+    companion object {
+        private val logger = KotlinLogging.logger {}
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val topic = "simple-topic"
+
+            val props = initConsumerProps(
+                keyDeSerClass = StringDeserializer::class.java,
+                valueDeSerClass = StringDeserializer::class.java
+            )
+
+            val consumer = KafkaConsumer<String, String>(props)
+            consumer.subscribe(listOf(topic))
+
+            val mainThread = Thread.currentThread()
+
+            // main thread 종료 시 별도의 thread로 kafka consumer wakeup() 호출
+            Runtime.getRuntime().addShutdownHook(thread(start = false) {
+                logger.info { "main program starts to exit by calling wakeup" }
+                consumer.wakeup()
+
+                // 메인 스레드가 종료될 때까지 대기
+                try {
+                    mainThread.join()
+                } catch (e: InterruptedException) {
+                    logger.error { e.message }
+                }
+            })
+
+            try {
+                while (true) {
+                    val consumerRecords = consumer.poll(Duration.ofMillis(1000))
+                    for (record in consumerRecords) {
+                        with(record) {
+                            logger.info { "key: ${key()}, value: ${value()}, partition: ${partition()}" }
+                        }
+                    }
+                }
+            } catch (e: WakeupException) {
+                logger.error { "wakeup exception has been called" }
+            } finally {
+                consumer.close()
+            }
+        }
+
+        private fun <K : Any, V : Any> initConsumerProps(
+            keyDeSerClass: Class<out Deserializer<K>>,
+            valueDeSerClass: Class<out Deserializer<V>>
+        ): Properties = Properties().apply {
+            put(BOOTSTRAP_SERVERS_CONFIG, "10.211.55.53:9092")
+            put(KEY_DESERIALIZER_CLASS_CONFIG, keyDeSerClass.name)
+            put(VALUE_DESERIALIZER_CLASS_CONFIG, valueDeSerClass.name)
+            put(GROUP_ID_CONFIG, "group_01")
+        }
+    }
+}
